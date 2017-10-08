58b24e82e973119a026960fa63791f8ccb4ea0eb
syu3 <syusan3@icloud.com>
2017-10-08 18:30:48 +0900

    /usersiteの追加

--- src/router/index.js
+++ src/router/index.js
@@ -2,20 +2,25 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Home from '@/components/Home'
-
+import userSite from '@/components/userSite'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/edit',
       name: 'Hello',
       component: Hello
     },
     {
-      path: '/home',
-      name: 'Home',
-      component: Home
+      path: '/usersite',
+      name: 'userSite',
+      component: userSite
     }
   ]
 })
